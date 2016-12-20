8f7c41673a51edcad6155af9439b9b692c0ee8d8
Haitong Zheng <Haitong@Haitong.home>
2016-12-20 13:58:34 -0500

    final

---
+++ scripts 2.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+  $('.new_pos').click(function() {
+    var randPercent = Math.random() * 100;
+    var randX = Math.random() * 100;
+    var randY = Math.random() * 100;
+    var randX2 = Math.random() * 100;
+    var randY2 = Math.random() * 100;
+    var randX3 = Math.random() * 100;
+    var randY3 = Math.random() * 100;
+    var randX4 = Math.random() * 100;
+    var randY4 = Math.random() * 100;
+    var randX5 = Math.random() * 100;
+    var randY5 = Math.random() * 100;
+    var randX6 = Math.random() * 100;
+    var randY6 = Math.random() * 100;
+    var randX7 = Math.random() * 100;
+    var randY7 = Math.random() * 100;
+    var randX8 = Math.random() * 100;
+    var randY8 = Math.random() * 100;
+    var randXm = Math.random() * 100;
+    var randYm = Math.random() * 100;
+    var randX9 = Math.random() * 100;
+    var randY9 = Math.random() * 100;
+    var randX10 = Math.random() * 100;
+    var randY10 = Math.random() * 100;
+    var randX11 = Math.random() * 100;
+    var randY11 = Math.random() * 100;
+    var randX12 = Math.random() * 100;
+    var randY12 = Math.random() * 100;
+    var randX13 = Math.random() * 100;
+    var randY13 = Math.random() * 100;
+    var randX14 = Math.random() * 100;
+    var randY14 = Math.random() * 100;
+    var randX15 = Math.random() * 100;
+    var randY15 = Math.random() * 100;
+
+    $('.img1').css('left', randX + "%");
+    $('.img1').css('top', randY + "%");
+    $('.img2').css('left', randX2 + "%");
+    $('.img2').css('top', randY2 + "%");
+    $('.img3').css('left', randX3 + "%");
+    $('.img3').css('top', randY3 + "%");
+    $('.img4').css('left', randX4 + "%");
+    $('.img4').css('top', randY4 + "%");
+    $('.img5').css('left', randX5 + "%");
+    $('.img5').css('top', randY5 + "%");
+    $('.img6').css('left', randX6 + "%");
+    $('.img6').css('top', randY6 + "%");
+    $('.img7').css('left', randX7 + "%");
+    $('.img7').css('top', randY7 + "%");
+    $('.img8').css('left', randX8 + "%");
+    $('.img8').css('top', randY8 + "%");
+    $(".morty").css('left', randXm + "%");
+    $(".morty").css('top', randYm + "%");
+    $('.img9').css('left', randX9 + "%");
+    $('.img9').css('top', randY9 + "%");
+    $('.img10').css('left', randX10 + "%");
+    $('.img10').css('top', randY10 + "%");
+    $('.img11').css('left', randX11 + "%");
+    $('.img11').css('top', randY11 + "%");
+    $('.img12').css('left', randX12 + "%");
+    $('.img12').css('top', randY12 + "%");
+    $('.img13').css('left', randX13 + "%");
+    $('.img13').css('top', randY13 + "%");
+    $('.img14').css('left', randX14 + "%");
+    $('.img14').css('top', randY14 + "%");
+    $('.img15').css('left', randX15 + "%");
+    $('.img15').css('top', randY15 + "%");
+
+
+
+
+  });
+$('.morty').click(function() {
+$("body").append("You found him! Yah");
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
